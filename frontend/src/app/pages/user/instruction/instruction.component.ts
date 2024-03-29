9bdcf9eadb543184110b646d24eac50e777a1b44
Janak Chaudhary <janakchy01@gmail.com>
2024-03-29 17:00:56 +0545

    quiz instruction page created

--- frontend/src/app/pages/user/instruction/instruction.component.ts
+++ frontend/src/app/pages/user/instruction/instruction.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { QuizService } from '../../admin/services/quiz.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-instruction',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatDividerModule, MatButtonModule],
   templateUrl: './instruction.component.html',
   styleUrl: './instruction.component.css',
 })
 export class InstructionComponent {
   qid: any;
-  constructor(private activeRoute: ActivatedRoute) {}
+  quiz: any;
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private quizService: QuizService
+  ) {}
   ngOnInit() {
     this.qid = this.activeRoute.snapshot.params['qid'];
+    this.quizService.getQuizById(this.qid).subscribe((data: any) => {
+      this.quiz = data;
+      console.log(this.quiz);
+    });
     console.log(this.qid);
   }
 }
